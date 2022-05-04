dd7b60b05a193c55bd616672f43618dc6074d645
Alexei <alexei.prudnikov@gmail.com>
2022-05-04 22:33:32 +0300

    Add Project UpgradeStringArray

--- UpgradeStringArray/Program.cs
+++ UpgradeStringArray/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int maxLength = 3; // максимальная длина слова, удовлетворяющая условию
+
+// Медод, принимает на вход массив строк words и 
+// возвращает количество слов длиной меньше либо равно length 
+int CountWordsLessLength(string[] words, int length)
+{
+    int count = 0;
+    foreach (string word in words)
+    {
+        if (word.Length <= length) count++;
+    }
+    return count;
+}
+// Медод, принимает на вход массив строк words и 
+// возвращает массив слов длиной меньше либо равно length 
+string[] WordsLessLength(string[] words, int length)
+{
+    string[] newWords = new string[CountWordsLessLength(words, length)];
+    int index = 0;
+    foreach (string word in words)
+    {
+        if (word.Length <= length)
+        {
+            newWords[index] = word;
+            index++;
+        } 
+    }
+    return newWords;
+}
+// Метод, осуществляющий вывод массива string на консоль
+void PrintArray(string[] array)
+{
+    foreach (string element in array)
+    {
+        Console.Write($"[{element}] ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите строку:");
+string str = Console.ReadLine();
+string[] words = str.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+PrintArray(WordsLessLength(words, maxLength));
